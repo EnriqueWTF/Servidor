cd00b5c23ac36aadf713d23e6e9618b4468278ae
Enrique <proenrique4@gmail.com>
2025-09-17 07:39:01 -0700

    recibir mensaje del cliente e imprimirlo

--- mmain.java
+++ mmain.java
@@ -1,4 +1,6 @@
 import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 public class mmain {
     
@@ -8,9 +10,20 @@ public class mmain {
   System.out.println("Esperando al cliente...");
   Socket cliente = servidor.accept();
   System.out.println("Cliente conectado");
-  cliente.close();
+  
+   BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+    String mensaje = entrada.readLine();
+    System.out.println("Cliente te dice: " + mensaje);
 
+
+
+    
+    servidor.close();
+    
+  cliente.close();
     }
+ 
+
     
 
     
