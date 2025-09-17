1d76775ac177a5d0aca40cceddae57c36e8b7067
Enrique <proenrique4@gmail.com>
2025-09-17 07:51:06 -0700

    Guardar el mensaje en el archivo de texto

--- mmain.java
+++ mmain.java
@@ -2,6 +2,10 @@ import java.net.Socket;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;  
+
 public class mmain {
     
     public static void main(String[] args) throws Exception {
@@ -15,10 +19,12 @@ public class mmain {
     String mensaje = entrada.readLine();
     System.out.println("Cliente te dice: " + mensaje);
 
+   BufferedWriter write = new BufferedWriter(new FileWriter("mensaje.txt"));
+    write.write(mensaje);
+   
+   write.close();
 
-
-    
-    servidor.close();
+  servidor.close();
     
   cliente.close();
     }
